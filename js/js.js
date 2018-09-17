70132ed28f9d0d666b2848b4e9a5c0d2c62187eb
anhnhat.nd96@gmail.com <anhnhat.nd96@gmail.com>
2018-09-17 15:28:43 +0700

    cn

--- js/js.js
+++ js/js.js
@@ -18,14 +18,15 @@ $(document).ready(function(){
                 $(".show-more").hide()
             });
         
-            $("a[href^='#']").on('click',function (e) {
-                e.preventDefault();
-                var target = this.hash;
-                $target = $(target);
-                $("html, body").stop().animate({
-                    'scrollTop':  $target.offset().top
-                }, 900, 'swing', function () {
-                    window.location.hash = target;
-                });
-            });
+            // $(".nav-link").on('click',function (e) {
+            //     e.preventDefault();
+            //     var target = this.hash;
+            //     $target = $(target);
+            //     $("html, body").stop().animate({
+            //         'scrollTop':  $target.offset().top
+            //     }, 900, 'swing', function () {
+            //         window.location.hash = target;
+            //     });
+            //     console.log(1);
+            // });
     })
